bb74b210edf890780ebc99718607feda88cbf85f
Kirill Groshev <kirill.groshev@phystech.edu>
2019-04-11 00:28:49 +0300

    added new words

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const title = document.querySelector(".title h1");
+const sign = document.querySelector(".title h2");
 
 window.addEventListener('load', () => {
     setRandomTitle();
@@ -6,6 +7,7 @@ window.addEventListener('load', () => {
 
 function setRandomTitle() {
     let pair = words[Math.floor(Math.random() * words.length)];
-    title.textContent = "Не " + pair[0] + " убивает человека, а человек " + pair[1];
+    title.textContent = "Не " + pair[0] + " убивает человека, а человек " + pair[1] + ".";
+    sign.textContent = "© В.И.Сталин";
     title.style.opacity = 1;
 }
